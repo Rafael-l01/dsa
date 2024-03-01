808e144ff2fc67dd303293699d5e612f409a8964
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-03-01 12:03:34 -0300

    feat: implement a hash map

---
+++ hash-tables/hash-map.py
@@ -0,0 +1,80 @@
+class HashMap:
+    def __init__(self, size: int = 7) -> None:
+        self.hashMap = [None] * size
+
+    def __hash(self, key: str):
+        myHash = 0
+        for letter in key:
+            myHash = (myHash + ord(letter) * 23) % len(self.hashMap)
+
+        return myHash
+
+    def setItem(self, key: str, value: int) -> None:
+        index = self.__hash(key)
+
+        if self.hashMap[index] == None:
+            self.hashMap[index] = []
+
+        found = False
+        for i, pair in enumerate(self.hashMap[index]):
+            recordKey, recordValue = pair
+
+            if recordKey == key:
+                found = True
+                self.hashMap[index][i] = [key, value]
+
+        if not found:
+            self.hashMap[index].append([key, value])
+
+    def getItem(self, key: str) -> int:
+        index = self.__hash(key)
+
+        if self.hashMap[index] is not None:
+            for pair in self.hashMap[index]:
+                recordKey, recordValue = pair
+
+                if recordKey == key:
+                    return recordValue
+
+        return None
+
+    def removeItem(self, key: str) -> None:
+        index = self.__hash(key)
+
+        if self.hashMap[index] is not None:
+            for i, pair in enumerate(self.hashMap[index]):
+                recordKey, recordValue = pair
+
+                if recordKey == key:
+                    del self.hashMap[index][i]
+                    return
+
+    def getKeys(self) -> list:
+        keys = []
+
+        for bucket in self.hashMap:
+            if bucket is not None:
+                for pair in bucket:
+                    recordKey, recordValue = pair
+                    keys.append(recordKey)
+
+        return keys
+
+    def __str__(self):
+        mapStr = "{"
+
+        isEmpty = True
+        for bucket in self.hashMap:
+            if bucket is not None:
+                for pair in bucket:
+                    isEmpty = False
+                    key, value = pair
+                    mapStr += key + ": " + str(value) + ", "
+
+        if isEmpty:
+            mapStr += "}"
+        else:
+            mapStr = mapStr[0:-2] + "}"
+
+        return mapStr
+
