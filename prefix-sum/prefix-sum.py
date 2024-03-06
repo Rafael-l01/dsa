af03ad42338c813433d2935e63eb79dfbfda7c30
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-03-05 22:20:11 -0300

    feat: add the basic prefix sum problem definition of how to get the sub arrays sums

--- prefix-sum/prefix-sum.py
+++ prefix-sum/prefix-sum.py
@@ -18,3 +18,18 @@ def prefixSum3(array):
     for i, num in enumerate(array):
         prefixSum.append(prefixSum[i] + num)
 
+
+class PrefixSumProblem:
+    def __init__(self, nums):
+        self.nums = nums
+        self.prefixSum = self.calculatePrefixSum()
+
+    def calculatePrefixSum(self):
+        prefixSum = [0] * 10001
+        for i in range(len(self.nums)):
+            prefixSum[i + 1] = prefixSum[i] + self.nums[i]
+
+        return prefixSum
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefixSum[right + 1] - self.prefixSum[left]
