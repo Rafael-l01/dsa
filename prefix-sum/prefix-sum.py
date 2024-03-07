e6427ae2cbf331bfe3b9912824c838c235be9560
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-03-07 12:48:37 -0300

    feat: add the basic prefix sum for 2d array problem definition of how to get a sub matrix sum

--- prefix-sum/prefix-sum.py
+++ prefix-sum/prefix-sum.py
@@ -49,3 +49,33 @@ def prefixSum2D(matrix):
 
     return prefixSum
 
+
+class PrefixSumProblem2D:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.prefixSum = self.calculatePrefixSum2D()
+
+    def calculatePrefixSum2D(self):
+        prefixSum = [
+            [0] * (len(self.matrix[0]) + 1) for i in range(len(self.matrix) + 1)
+        ]
+
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                prefixSum[i + 1][j + 1] = (
+                    prefixSum[i + 1][j]
+                    + prefixSum[i][j + 1]
+                    - prefixSum[i][j]
+                    + self.matrix[i][j]
+                )
+
+        return prefixSum
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return (
+            self.prefixSum[row2 + 1][col2 + 1]
+            - self.prefixSum[row2 + 1][col1 - 1 + 1]
+            - self.prefixSum[row1 - 1 + 1][col2 + 1]
+            + self.prefixSum[row1 - 1 + 1][col1 - 1 + 1]
+        )
+
